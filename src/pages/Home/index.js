6353bd46d531e583956358d07d45de8d9e935297
viictorhoffmann <viictorhoffmann@gmail.com>
2021-02-23 12:13:00 -0300

    feature: inicializando e conhecendo React

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+function Hello(){
+  return <h1>Hello World</h1>
+}
+
+function Home() {
+  return (
+    <div>
+      <Hello />
+      <h1>Victor Hoffmann</h1>
+    </div>
+  )
+}
+
+export default Home
